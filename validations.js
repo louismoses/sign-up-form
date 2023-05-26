2f32a0683d1accb08278ef955661ef166442424e
Louis Moses <louismoses.rait@gmail.com>
2023-05-26 17:34:20 +0800

    Update : limit max character for name

--- validations.js
+++ validations.js
@@ -34,9 +34,26 @@ firstName.onkeyup = (e) => {
     firstName.nextElementSibling.style.color = "red";
     firstName.previousElementSibling.classList.remove("d-none");
     firstName.previousElementSibling.innerHTML = "Maximum of 20 character";
-    validate.firstname = true;
+    validate.firstname = false;
   } else {
-    console.log("moses");
+    firstName.style.color = "green";
+    firstName.nextElementSibling.style.color = "green";
+    firstName.previousElementSibling.classList.add("d-none");
+    validate.firstname = true;
+  }
+};
+middleName.onkeyup = (e) => {
+  if (e.target.value.length > 20) {
+    middleName.style.color = "red";
+    middleName.nextElementSibling.style.color = "red";
+    middleName.previousElementSibling.classList.remove("d-none");
+    middleName.previousElementSibling.innerHTML = "Maximum of 20 character";
+    validate.middlename = false;
+  } else {
+    middleName.style.color = "green";
+    middleName.nextElementSibling.style.color = "green";
+    middleName.previousElementSibling.classList.add("d-none");
+    validate.middlename = true;
   }
 };
 lastName.onkeyup = (e) => {
@@ -45,8 +62,11 @@ lastName.onkeyup = (e) => {
     lastName.nextElementSibling.style.color = "red";
     lastName.previousElementSibling.classList.remove("d-none");
     lastName.previousElementSibling.innerHTML = "Maximum of 20 character";
-    validate.lastname = true;
+    validate.lastname = false;
   } else {
-    console.log("moses");
+    lastName.style.color = "green";
+    lastName.nextElementSibling.style.color = "green";
+    lastName.previousElementSibling.classList.add("d-none");
+    validate.lastname = true;
   }
 };
