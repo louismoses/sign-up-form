48eda26042e497268799e7370e20de2ff42d1ec3
Louis Moses <louismoses.rait@gmail.com>
2023-05-26 17:28:55 +0800

    Update : limit max character for name

--- validations.js
+++ validations.js
@@ -27,3 +27,26 @@ loginForm.addEventListener("submit", function (e) {
     loginForm.submit();
   }
 });
+
+firstName.onkeyup = (e) => {
+  if (e.target.value.length > 20) {
+    firstName.style.color = "red";
+    firstName.nextElementSibling.style.color = "red";
+    firstName.previousElementSibling.classList.remove("d-none");
+    firstName.previousElementSibling.innerHTML = "Maximum of 20 character";
+    validate.firstname = true;
+  } else {
+    console.log("moses");
+  }
+};
+lastName.onkeyup = (e) => {
+  if (e.target.value.length > 20) {
+    lastName.style.color = "red";
+    lastName.nextElementSibling.style.color = "red";
+    lastName.previousElementSibling.classList.remove("d-none");
+    lastName.previousElementSibling.innerHTML = "Maximum of 20 character";
+    validate.lastname = true;
+  } else {
+    console.log("moses");
+  }
+};
